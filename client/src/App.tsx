b71d48a12b61d2c7c3c7c2b2509fb8ffa780a0c0
umar-developer-01 <umarkhandev9@gmail.com>
2024-11-25 23:56:15 +0530

    Chnages in socket connection

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,23 +2,32 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  
   const [socket, setSocket] = useState<WebSocket | null>(null);
 
   useEffect(() => {
     const newSocket = new WebSocket("ws://localhost:8080");
     newSocket.onopen = () => {
       console.log("Connection established");
+      setSocket(newSocket);
       newSocket.send("Hello Server from Client!");
     };
     newSocket.onmessage = (message) => {
       console.log("Message received:", message.data);
     };
-    setSocket(newSocket);
+
     return () => newSocket.close();
   }, []);
 
-  return <>hi there</>;
+  if (!socket) {
+    return (
+      <>
+        <div>Socket connecting...</div>
+      </>
+    );
+  }
+  return <>Hi there socker connection established</>;
 }
 
 export default App;
+
+// now use the socket state to send the data or pass it in Context API or redux and use it any where
